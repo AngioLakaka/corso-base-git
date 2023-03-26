145aa90b14a7c793e96cfccdafdf7c1032be803a
AngioLakaka <116262260+AngioLakaka@users.noreply.github.com>
2023-03-26 12:30:29 +0200

    Add files via upload

--- equazioni.py
+++ equazioni.py
@@ -3,8 +3,11 @@ import math
 a_valore=0
 b_valore=0
 c_valore=0
+def delta_eqz(a=int,b=int,c=int):
+    risultato=int((b*b)-4*(a*c))
+    return  risultato
 print("EQUAZIONI DI SECONDO GRADO")
-#assicuro che ci sia almeno un valore positivo
+#assicuro che ci sia almeno un valore diverso da 0
 while (a_valore == 0 and b_valore == 0 and c_valore == 0):
     print("inserisci almeno un valore diverso da 0")
     a_valore=int(input("Inserisci il valore di a: "))
@@ -14,21 +17,21 @@ while (a_valore == 0 and b_valore == 0 and c_valore == 0):
 #restituisco in output il tipo di equazione in base ai valori inseriti e scrivo l'equazione
 if(c_valore == 0):
     print("Questa è spuria")
-    print(("L'equazione è ") + (str(a_valore)) + ("x2") + (" + ") + str(b_valore) +("x")  + (" = 0"))
+    print(("L'equazione è ") + (str(a_valore)) + ("x^2") + (" + ") + str(b_valore) +("x")  + (" = 0"))
 if(b_valore == 0):
     print("Questa è pura")
-    print(("L'equazione è ") + (str(a_valore)) + ("x2")  + (" + ") + str(c_valore) + (" = 0"))
+    print(("L'equazione è ") + (str(a_valore)) + ("x^2")  + (" + ") + str(c_valore) + (" = 0"))
 if(b_valore == 0 and c_valore == 0):
     print("Questa è monomia")
-    print(("L'equazione è ") + (str(a_valore)) + ("x2")  + (" = 0"))
-if(a_valore>0 and b_valore>0 and c_valore>0):
+    print(("L'equazione è ") + (str(a_valore)) + ("x^2")  + (" = 0"))
+if(a_valore!=0 and b_valore!=0 and c_valore!=0):
     print("Questa è completa")
-    print(("L'equazione è ") + (str(a_valore)) + ("x2") + (" + ") + str(b_valore) +("x") + (" + ") + str(c_valore) + (" = 0"))
+    print(("L'equazione è ") + (str(a_valore)) + ("x^2") + (" + ") + str(b_valore) +("x") + (" + ") + str(c_valore) + (" = 0"))
 #applico le formule per la risoluzione
-delta= (b_valore*b_valore)-4*(a_valore * c_valore)
-print("Faccio il delta") 
-print("Il delta è " + str(delta))    
-if(delta < 0):
+print("Faccio il delta")
+delta=(delta_eqz(a_valore,b_valore,c_valore))
+print("Il delta è "+ str(delta))
+if(delta<0):
     print("L'equazione è impossibile")
 if(delta == 0):
     print("L'equazione ha 2 soluzioni reali e coincidenti")
